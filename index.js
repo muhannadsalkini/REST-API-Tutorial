8542444b98b6282e1c2e38b2264e51b0ad545252
MUHANNAD SALKINI <mohanad.salkini@gmail.com>
2023-08-07 14:24:43 +0300

    006 Error Handling

--- index.js
+++ index.js
@@ -9,9 +9,15 @@ const app = express();
 mongoose.connect("mongodb://localhost:27017/ninjago");
 mongoose.Promise = global.Promise;
 
-app.use(bodyParser.json());
+app.use(bodyParser.json()); // middleware
 
 app.use("/api", require("./routes/api")); // require the api and use it
+
+//error handling middleware
+app.use(function (err, req, res, next) {
+  //   console.log(err);
+  res.status(422).send({ error: err.message });
+});
 
 // listen for request
 app.listen(process.env.port || 4000, function () {
--- routes/api.js
+++ routes/api.js
@@ -5,28 +5,30 @@ const Ninja = require("../models/ninja"); // import Ninja model
 const router = express.Router(); // router object
 
 // get a lost of ninjas from the db
-router.get("/ninjas", function (req, res) {
+router.get("/ninjas", function (req, res, next) {
   res.send({ type: "GET" });
 });
 
 // add a new ninja to the db
-router.post("/ninjas", function (req, res) {
+router.post("/ninjas", function (req, res, next) {
   //   var ninja = new Ninja(req.body);
   //   ninja.save(); // save in the database
 
   // create object and save
-  Ninja.create(req.body).then(function (ninja) {
-    res.send(ninja); // response with the saved data
-  });
+  Ninja.create(req.body)
+    .then(function (ninja) {
+      res.send(ninja); // response with the saved data
+    })
+    .catch(next);
 });
 
 // update a ninja in the db
-router.put("/ninjas/:id", function (req, res) {
+router.put("/ninjas/:id", function (req, res, next) {
   res.send({ type: "PUT" });
 });
 
 // delete a ninja from the db
-router.delete("/ninjas/:id", function (req, res) {
+router.delete("/ninjas/:id", function (req, res, next) {
   res.send({ type: "DELETE" });
 });
 
