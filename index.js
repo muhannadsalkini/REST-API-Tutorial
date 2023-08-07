d8c787415437cc44b2737fe3f8e3c60713c9fc7c
MUHANNAD SALKINI <mohanad.salkini@gmail.com>
2023-08-07 11:00:05 +0300

    003 Handling POST Requests (& middleware)

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require("express"); // require express
+const bodyParser = require("body-parser"); // require body parser middleware to handle data (post, put ..)
 
 // set up express app
 const app = express();
+
+app.use(bodyParser.json());
 
 app.use("/api", require("./routes/api")); // require the api and use it
 
